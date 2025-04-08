351785134370fc44f61c00a9a8e22aad9c304a35
albertopane <alberto.pane@hotmail.com>
2025-04-08 16:11:47 +0200

    Add files via upload

---
+++ 07-04/cicli.py
@@ -0,0 +1,19 @@
+#while esempio 1
+controllore = True
+
+while controllore:
+    print(1)
+    controllore = False
+
+#while esempio 2
+conteggio = 0
+
+while conteggio < 5:
+    print(conteggio)
+    conteggio +=1
+
+#ciclo for
+numeri = [1, 2, 3, 4, 5]
+
+for numero in numeri:
+    print(numero)
---
+++ 07-04/es3.py
@@ -0,0 +1,21 @@
+#creazione variabili
+
+id = 0
+x = True
+
+while x:
+    nome = ""
+    password = ""
+
+    nome = input("inserisci il tuo nickname: ")
+    password = input("Inserisci la tua password: ")
+    id +=1
+
+    #condizioni
+    if nome == "":
+        print("Non hai valorizzato.")
+    else:
+        nome_inserito = input("Inserisci il tuo nickname:")
+        password_inserita = input("Inserisci la tua password: ")
+        if nome_inserito == nome and password_inserita == password:
+            print("Sei loggato.")
